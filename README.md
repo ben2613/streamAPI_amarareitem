e0dec8f0e5a4faf85c6c91769df9cb7cf7c2440c
Ben Wong <ben.wong@vesta-technology.com>
2017-04-06 18:08:19 +0800

    Add PushBullet channel

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This continuously grab tweet from @amarareitem and open the url which are my target items.
+Pre-requirement:
+[pushbullet.py](https://github.com/randomchars/pushbullet.py)
+[Twitter for Python!](https://github.com/tweepy/tweepy)
+
+
+This continuously grab tweet from @amarareitem and open the url which are target items.
--- refresh_twitter.py
+++ refresh_twitter.py
@@ -7,8 +7,17 @@ import urllib.request
 import sys
 import re
 import webbrowser
+from pushbullet import PushBullet
 
 class StdOutListener(StreamListener):
+    def __init__(self,pbAPI):
+        self.channel = None
+        if pbAPI.strip():
+            self.pb = PushBullet(pbAPI)
+            self.channel = self.pb.channels[0]
+            print('Pushbullet used')
+        pass
+    
     def on_data(self, data):
         tweet = json.loads(data)
         print('--------------------------------------------------')
@@ -26,20 +35,22 @@ class StdOutListener(StreamListener):
             urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tweet['text'])
             for url in urls:
                 webbrowser.open(url)
+                self.channel.push_link(text, url)
 
 if __name__ == '__main__':
     if len(sys.argv) is not 1:
         with open('example.json', encoding='utf-8') as f:
             tweet = json.loads(f.read())
         print(tweet['text'])
-        StdOutListener.auto_open('1',tweet)
+        l = StdOutListener(cred.get('pb_api'))
+        l.auto_open(tweet)
         exit()
 
     #Variables of OAuth
-    with open('credential') as f:
+    with open('credential.ini') as f:
         cred = dict(line.split('=') for line in f.read().splitlines())
 
-    l = StdOutListener()
+    l = StdOutListener(cred.get('pb_api'))
     auth = OAuthHandler(cred['api_key'], cred['api_secret'])
     auth.set_access_token(cred['access_token'], cred['access_token_secret'])
     stream = Stream(auth, l)
